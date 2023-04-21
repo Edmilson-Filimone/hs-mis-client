9605912d4035d12134a37f55028781b109b9b6f7
edmilson-filimone <philimone99@gmail.com>
2023-04-21 12:36:35 +0200

    Creating the filter form in NavBar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,24 @@ import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router'
 import { auth } from '../../firebase.config'
 import { toast } from 'react-toastify'
+import { AiOutlineClose } from 'react-icons/ai'
 
 function Navbar() {
+  //Form States and others hooks
+  const[pathData,setPathData] = useState()  
+  const [display, setDisplay] = useState('hidden')
+  const [filterInput, setFilterInput] = useState('district')
   const location = useLocation()
   const navigate = useNavigate()
+
+  //On Submit function for Filter form
+  const onSubmit = (e)=>{
+    e.preventDefault()
+    console.log(filterInput)
+    //applyFilter(filterInput)
+  }
+
+  //Function to get the name of the current page/path
   const path = ()=>{
     let pathname = location.pathname.split('/').join(' / ') //just splited and joined for text style formating propose only
     let shrinkpathname = pathname.split('/')[1]
@@ -25,13 +39,10 @@ function Navbar() {
     return {pathname, name}
   }
   
-const[pathData,setPathData] = useState()  
 useEffect(()=>{
   const {pathname, name} = path()
   setPathData({pathname, name})
 },[location.pathname])
-
-  
 
   //Scroll event for conditional styling
   const [onScroll, setOnScroll] = useState(false)
@@ -66,9 +77,21 @@ useEffect(()=>{
         </form>
         <div className='flex justify-between shrink-0 w-[100px] cursor-pointer'>
             <UserCircleIcon className={`${onScroll ? 'text-title-color' : 'text-gray-500'}`} width={"20px"} />
-            <Cog6ToothIcon className={`${onScroll ? 'text-title-color' : 'text-gray-500'}`} width={"20px"}/>
+            <Cog6ToothIcon className={`${onScroll ? 'text-title-color' : 'text-gray-500'}`} width={"20px"} onClick={()=>{setDisplay('block')}} />
             <PowerIcon className={`${onScroll ? 'text-title-color' : 'text-gray-500'}  hover:text-red-600`} title='sign out' width={"20px"} onClick={signOut}/>
         </div>
+      </div>
+      <div className={`${display} absolute w-[300px] h-[270px] p-4 shadow-lg border rounded-lg bg-[rgba(255,255,255,0.9)] backdrop-blur top-2 md:right-2`}>
+        <button className='block float-right text-lg font-semibold' onClick={()=>{setDisplay('hidden')}}>{<AiOutlineClose/>}</button>
+        <h4 className='font-bold text-title-color text-center mb-10 border-b pb-6'>Apply Filter</h4>
+          <form className='flex flex-col gap-4' onSubmit={onSubmit}>
+            <select className='text-text-color bg-white p-1.5 border mb-4' name="filter" id="filter" onChange={(e)=> setFilterInput(e.target.value)}>
+              <option value="province">Province</option>
+              <option value="district">District</option>
+              <option value="facility">Facility</option>
+            </select>
+            <input className='block bg-light border p-2 w-full' id='submit' type="submit" value="Select"/>
+          </form>
       </div>
     </nav>
   )
